1752006f248e954fefec022bb81a8adfb6d0655f
Quinton Fults <quinton.fults.work@gmail.com>
2021-06-11 10:36:14 -0700

    code

---
+++ sql/seed.sql
@@ -0,0 +1,8 @@
+USE todo_db;
+
+INSERT INTO items (text, isDone)
+VALUES ('take out trash', false),
+('do laundry', false),
+('cook dinner', true),
+('walk the dog', false),
+('pick up dry cleaning', true);
